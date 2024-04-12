c5b081f16ed9d63a5fb4fcdc7562c908b995073c
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-04-13 02:37:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.16%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,40 +1,24 @@
-						// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-		// resultant string
-        vector<string> result;
-        
-        int n = nums.size();
-		// if size happens to be  zero return empty string
-        if(n == 0 )
-                return result;
-        
-		// assigning first element to a
-        int a = nums[0];
-        
-        for(int i = 0; i<n; i++)
-        {
-			// if one of both is true
-            if( i == n-1 || nums[i]+1 != nums[i+1])
-            {
-			    // if current element is not equals a
-				// this means we have found a range.
-                if(nums[i] != a)
-                    result.push_back(to_string(a)+ "->"+ to_string(nums[i]));
-					
-				// this means we have reached to the end of string and now
-				// we have to add a that should be the last element
+        vector<string> rang;
+        int n=nums.size();       
+        if(n==0)
+           return rang;
+        int start=nums[0];
+        for(int i=0;i<n;i++){
+            if(i==n-1 || nums[i]+1 != nums[i+1]){
+                if(nums[i]!=start){
+                   rang.push_back(to_string(start)+"->"+ to_string(nums[i]));
+                }
                 else
-                        result.push_back(to_string(a));
-						
-				// checking  for this condition so that a got updated for next range
-				// also n-1 so that a doesn't contain out of bound value
+                   rang.push_back(to_string(start));
+                
                 if(i != n-1)
-                    a = nums[i+1];
+                   start=nums[i+1];
+                   
             }
         }
-		// return result
-        return result;
+        return rang;
     }
 };
