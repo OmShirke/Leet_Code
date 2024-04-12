8faea4905dc535b920a910e7ea6b78d334d606dc
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-04-12 21:21:31 +0530

    Time: 0 ms (100%), Space: 10.3 MB (15.37%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        int i;
+        for(i=n-1;i>=0;i--){
+            if(digits[i]<9){
+               digits[i]++;
+               return digits;
+            }
+            else{
+                digits[i]=0; 
+            }       
+        }
+        digits.insert(digits.begin(),1);
+    return digits;
+    }
+};
