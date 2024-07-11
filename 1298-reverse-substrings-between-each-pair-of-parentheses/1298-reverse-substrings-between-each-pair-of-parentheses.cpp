cd53b97622747b77fd159e173a057eeff401c81b
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-07-11 20:38:55 +0530

    Time: 0 ms (100%), Space: 7.7 MB (60.32%) - LeetHub

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> st; 
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                int j = st.top();
+                st.pop();
+                reverse(s.begin() + j + 1, s.begin() + i);
+            }
+        }
+        string ans;
+        for (char c : s) {
+            if (c != '(' && c != ')') {
+                ans += c;
+            }
+        }
+        
+        return ans;
+    }
+};
