82d47acc82030bd800fc3e6a796c78b4263418a4
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-07-16 03:37:02 +0530

    Time: 0 ms (100%), Space: 7.4 MB (14.53%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        int prev1=1;
+        int prev2=1;
+        for(int i=2;i<=n;i++){
+            int current=prev1+prev2;
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
+};
