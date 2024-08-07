1bc07e059c97e848f4b9cb214b0a41adccfd53a1
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-08-07 22:20:16 +0530

    Time: 48 ms (41.17%), Space: 51.2 MB (18.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int farthest = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i > farthest) {
+                return false;
+            }
+            farthest = max(farthest, i + nums[i]); 
+        }
+        
+        return true;  
+    }
+};
