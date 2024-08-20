6446ec85a2107ab4fba1650ef557d2cc1bc12cf2
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-08-20 23:56:30 +0530

    Time: 12 ms (78.46%), Space: 20.3 MB (46.75%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> arr(n,1);
         for(int i=n-1;i>0;i--){
             if(ratings[i]<ratings[i-1]){
-               arr[i-1]=max(arr[i-1],arr[i]+1);
+               arr[i-1]=arr[i]+1;
             }
         }
         for(int i=0;i<n-1;i++){
