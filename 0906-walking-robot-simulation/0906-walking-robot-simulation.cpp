9d988cc7d6b6fb78a450764ba4726cd0b76fca2a
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-09-05 00:28:17 +0530

    Time: 113 ms (31.49%), Space: 39.6 MB (59.67%) - LeetHub

---
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int x = 0, y = 0, direction = 0, maxDist = 0;
+        
+        unordered_set<string> obstacleSet;
+        for (const auto& obs : obstacles) {
+            obstacleSet.insert(to_string(obs[0]) + "," + to_string(obs[1]));
+        }
+
+        for (int command : commands) {
+            if (command == -1) {
+                direction = (direction + 1) % 4;  
+            } else if (command == -2) {
+                direction = (direction + 3) % 4; 
+            } else {
+                for (int i = 0; i < command; ++i) {
+                    int nextX = x + directions[direction].first;
+                    int nextY = y + directions[direction].second;
+                    if (obstacleSet.find(to_string(nextX) + "," + to_string(nextY)) == obstacleSet.end()) {
+                        x = nextX;
+                        y = nextY;
+                        maxDist = max(maxDist, x * x + y * y);
+                    } else {
+                        break; 
+                    }
+                }
+            }
+        }
+        return maxDist;
+    }
+};
