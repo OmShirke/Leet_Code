b0219d103e0ee3677006b722c9e3f3733eaf0899
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-12-02 16:04:21 +0530

    Time: 0 ms (100%), Space: 8.2 MB (7.8%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -4,35 +4,17 @@ public:
         int n = search.size();
         string word = "";
         int ans = 0;
-        for (int i = 0; i < sentence.size(); i++) {
-            if (sentence[i] != ' ') {
+        for (int i = 0; i <= sentence.size(); i++) {
+            if (i < sentence.size() && sentence[i] != ' ') {
                 word += sentence[i];
-            } else if (word.size() >= n) {
-                ans++;
-                for (int j = 0; j < n; j++) {
-                    if (word[j] != search[j]) {
-                        word = "";
-                        break;
-                    }
-                    if (j == n - 1) {
-                        return ans;
-                    }
-                }
             } else {
                 ans++;
-                word = "";
+                if (word.size() >= n && word.substr(0, n) == search) {
+                    return ans;
+                }
+                word="";
             }
         }
-        ans++;
-        for (int j = 0; j < n; j++) {
-            if (word[j] != search[j]) {
-                break;
-            }
-            if (j == n - 1) {
-                return ans;
-            }
-        }
-
         return -1;
     }
 };
