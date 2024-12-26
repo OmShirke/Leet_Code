739522ac113de7fbf99d15f5a37b9853bb0b9c17
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2024-12-26 12:48:21 +0530

    Time: 0 ms (100%), Space: 12.4 MB (61.72%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int s) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (s > sum || (s + sum) & 1) return 0;  // Ensure s is valid
+        if (s > sum || (s + sum) & 1) return 0; 
         return subsetSum(nums, (s + sum) >> 1);
     }
 
     int subsetSum(vector<int>& nums, int s) {
-        if (s < 0) return 0;  // Sanity check
+        if (s < 0) return 0; 
         vector<int> dp(s + 1, 0);
         dp[0] = 1;
         for (int n : nums) {
