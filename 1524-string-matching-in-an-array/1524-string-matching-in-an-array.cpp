0c33868b3e943b943480054099c148d0952cdf2f
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2025-01-07 14:47:23 +0530

    Time: 0 ms (100%), Space: 11.7 MB (29.52%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -8,7 +8,7 @@ public:
         vector<string> ans;
         for(int i=0;i<words.size()-1;i++){
             for(int j=i+1;j<words.size();j++){
-                if(words[j].find(words[i])!=string::npos){
+                if(words[j].find(words[i])!=-1){
                     ans.push_back(words[i]);
                     break;
                 }
