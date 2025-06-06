9efc395a9a60ee01c3cdec659eea055b9c77f662
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2025-06-07 05:14:20 +0530

    Time: 31 ms (92.41%), Space: 32.7 MB (63.71%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    // Helper function to find the lexicographically smallest remaining character
     char l(vector<int>& freq) {
         for (int i = 0; i < 26; i++) {
             if (freq[i]) return 'a' + i;
@@ -13,24 +12,20 @@ public:
         string t = "";
         vector<int> freq(26);
 
-        // Count frequency of each character in s
         for (char ch : s) {
             freq[ch - 'a']++;
         }
 
-        // Iterate over characters in s
         for (char ch : s) {
             st.push(ch);
             freq[ch - 'a']--;
 
-            // Pop from stack to result if stack top ≤ smallest remaining char
             while (!st.empty() && st.top() <= l(freq)) {
                 t += st.top();
                 st.pop();
             }
         }
 
-        // Pop remaining characters
         while (!st.empty()) {
             t += st.top();
             st.pop();
