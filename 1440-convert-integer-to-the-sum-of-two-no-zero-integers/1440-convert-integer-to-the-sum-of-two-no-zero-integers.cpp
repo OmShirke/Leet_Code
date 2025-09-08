2e63a95e16f2f9092f3437d3603a02a08842cfc2
Om Shirke <113794365+OmShirke@users.noreply.github.com>
2025-09-09 00:08:44 +0530

    Time: 0 ms (100%), Space: 8.2 MB (85.27%) - LeetHub

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasZero(int x) {
+        while (x > 0) {
+            if (x % 10 == 0) return true;
+            x /= 10;
+        }
+        return false;
+    }
+
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (!hasZero(a) && !hasZero(b)) {
+                return {a, b};
+            }
+        }
+        return {};
+    }
+};
